c6393d5eccc668ff1bf09e3c9b05afd84b8c1abf
Tomáš Hübelbauer <Tomas@Hubelbauer.net>
2016-07-16 15:28:53 +0200

    Scaffold Visual Studio solution

---
+++ src/Rhaeo.Ago/Rhaeo.Ago/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rhaeo.Ago.MvcApplication" Language="C#" %>
---
+++ src/Rhaeo.Ago/Rhaeo.Ago/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rhaeo.Ago.Startup))]
+namespace Rhaeo.Ago
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
